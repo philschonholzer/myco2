43cb0cf84798c3f350bc64c81044fe6b0a8a83e9
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2019-11-27 13:41:36 +0100

    Scale without change height of body

--- src/pages/index.js
+++ src/pages/index.js
@@ -161,46 +161,57 @@ const IndexPage = () => {
         <Container>
           <h2>Your co2e spending plan</h2>
           <p>How do you want to spend your co2e budget?</p>
-          <svg
-            viewBox={`-10 ${minmax(-500, 10, 50 - co2e * 5)} ${minmax(
-              120,
-              500,
-              yearsToZero(co2e) * 5 + 20
-            )} ${minmax(60, 200, co2e * 5 + 20)}`}
-            id="curve"
+          <div
+            css={css`
+              position: relative;
+              height: 50vw;
+            `}
           >
-            <text
-              x="-30"
-              y="-3"
-              transform="rotate(-90)"
-              style={{ fontSize: 7 }}
+            <svg
+              css={css`
+                position: absolute;
+                bottom: 0;
+              `}
+              viewBox={`-10 ${minmax(-500, 10, 50 - co2e * 5)} ${minmax(
+                120,
+                500,
+                yearsToZero(co2e) * 5 + 20
+              )} ${minmax(60, 200, co2e * 5 + 20)}`}
+              id="curve"
             >
-              co2e
-            </text>
-            <text x="50" y="67" style={{ fontSize: 7 }}>
-              years
-            </text>
-            <path d="M0,-500 L0,65 M-5,60 L500,60" stroke="#555" />
-            <path
-              d={`M0,${60 - co2e * 5} L${yearsToZero(co2e) * 5},60 L0,60 Z`}
-              stroke="#6af"
-              fill="#6af3"
-            />
-            <text y="40" style={{ fontSize: 7, fill: '#6af' }}>
-              <tspan x="5" dy="1.2em">
-                52,5t
-              </tspan>
-              <tspan x="5" dy="1.3em" style={{ fontSize: 4.5 }}>
+              <text
+                x="-30"
+                y="-3"
+                transform="rotate(-90)"
+                style={{ fontSize: 7 }}
+              >
                 co2e
-              </tspan>
-            </text>
-            <path d="M5,30 L90,30" stroke="#aaa" />
-            <rect x="40" y="22" width="15" height="15" fill="white" />
-            {/* eslint-disable-next-line jsx-a11y/accessible-emoji */}
-            <text x="40" y="36">
-              🇨🇭
-            </text>
-          </svg>
+              </text>
+              <text x="50" y="67" style={{ fontSize: 7 }}>
+                years
+              </text>
+              <path d="M0,-500 L0,65 M-5,60 L500,60" stroke="#555" />
+              <path
+                d={`M0,${60 - co2e * 5} L${yearsToZero(co2e) * 5},60 L0,60 Z`}
+                stroke="#6af"
+                fill="#6af3"
+              />
+              <text y="40" style={{ fontSize: 7, fill: '#6af' }}>
+                <tspan x="5" dy="1.2em">
+                  52,5t
+                </tspan>
+                <tspan x="5" dy="1.3em" style={{ fontSize: 4.5 }}>
+                  co2e
+                </tspan>
+              </text>
+              <path d="M5,30 L90,30" stroke="#aaa" />
+              <rect x="40" y="22" width="15" height="15" fill="white" />
+              {/* eslint-disable-next-line jsx-a11y/accessible-emoji */}
+              <text x="40" y="36">
+                🇨🇭
+              </text>
+            </svg>
+          </div>
           <label htmlFor="yearOneCo2">
             Amount of co2e this year
             <p>
