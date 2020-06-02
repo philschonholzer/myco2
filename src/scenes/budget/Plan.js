815d9301028c69d369b1c0cc014862f9b7b0c5fa
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2020-06-02 16:49:37 +0200

    Migrated plan to VX

--- src/scenes/budget/Plan.js
+++ src/scenes/budget/Plan.js
@@ -1,18 +1,121 @@
 import React from 'react'
 import { css } from '@emotion/core'
+
 import { addDays, format } from 'date-fns/fp'
 import { useScrollPercentage } from 'react-scroll-percentage'
 
-import { Section, Container, Range } from '../../style'
+import { AxisLeft, AxisBottom } from '@vx/axis'
+import { withParentSize } from '@vx/responsive'
+import { AreaClosed } from '@vx/shape'
+import { scaleLinear, scaleTime } from '@vx/scale'
+import { LinearGradient } from '@vx/gradient'
+import { Group } from '@vx/group'
+import { Text } from '@vx/text'
 
+import { Section, Container, Range } from '../../style'
 import { minmax, yearsToZero, positionY } from './lib'
 
-const Plan = ({ co2e, setCo2e, countryData, worldData }) => {
-  const [refPlan, planVisible] = useScrollPercentage()
+const margin = { top: 20, bottom: 40, left: 60, right: 20 }
+const x = d => d.date
+const y = d => d.co2e
 
+// Compose together the scale and accessor functions to get point functions
+const compose = (scale, accessor) => data => scale(accessor(data))
+
+const height = 500
+const width = 500
+
+// Then we'll create some bounds
+const xMax = width - margin.left - margin.right
+const yMax = height - margin.top - margin.bottom
+
+const xScale = scaleTime({
+  rangeRound: [0, xMax],
+  domain: [Date.now(), new Date('2050-01-01')],
+  nice: true,
+})
+
+const yScale = scaleLinear({
+  rangeRound: [yMax, 0],
+  domain: [0, 20],
+  nice: true,
+})
+
+const xPoint = compose(xScale, x)
+const yPoint = compose(yScale, y)
+
+const Graph = ({ co2e, endDate, countryData, worldData }) => {
+  const data = [
+    { co2e, date: Date.now() },
+    { co2e: 0, date: endDate },
+  ]
   const { Entity: name, Code: code, tCO2, Flags: flag } = countryData
   const { tCO2: tCO2World, Flags: flagWorld } = worldData
-  const y = positionY(45, 5)
+
+  return (
+    <svg width={width} height={height}>
+      <LinearGradient from="#fbc2eb" to="#a6c1ee" id="gradient" />
+      <Group top={margin.top} left={margin.left}>
+        <AreaClosed
+          data={data}
+          x={xPoint}
+          y={yPoint}
+          yScale={yScale}
+          fill="url(#gradient)"
+          stroke="#999"
+          strokeWidth={1}
+        />
+        <AxisLeft
+          scale={yScale}
+          top={0}
+          left={0}
+          label="Greenhouse gas (tCO2e)"
+          stroke="#1b1a1e"
+          tickTextFill="#1b1a1e"
+        />
+        <AxisBottom
+          scale={xScale}
+          top={yMax}
+          label="Year"
+          numTicks={8}
+          stroke="#1b1a1e"
+          tickTextFill="#1b1a1e"
+        />
+      </Group>
+      <path
+        d={`M60,${yPoint({ co2e: tCO2 })} L${xMax},${yPoint({ co2e: tCO2 })}`}
+        stroke="#aaa"
+      />
+      <Text
+        x={(xMax - margin.left) / 2}
+        y={yPoint({ co2e: tCO2 }) + 12}
+        fontSize="30"
+        textAnchor="middle"
+      >
+        {flag}
+      </Text>
+      <path
+        d={`M60,${yPoint({ co2e: tCO2World })} L${xMax},${yPoint({
+          co2e: tCO2World,
+        })}`}
+        stroke="#aaa"
+      />
+      <Text
+        x={(xMax - margin.left) / 2}
+        y={yPoint({ co2e: tCO2World }) + 12}
+        fontSize="30"
+        textAnchor="middle"
+      >
+        {flagWorld}
+      </Text>
+    </svg>
+  )
+}
+
+const Plan = ({ co2e, setCo2e, countryData, worldData }) => {
+  // const [refPlan, planVisible] = useScrollPercentage()
+  const endDate = addDays(yearsToZero(co2e) * 365, new Date())
+  const formattedEndDate = format('dd.MM.yyyy', endDate)
 
   return (
     <>
@@ -22,96 +125,31 @@ const Plan = ({ co2e, setCo2e, countryData, worldData }) => {
           <p>How do you want to spend your co2e budget?</p>
           <div
             css={css`
-              position: relative;
-              height: 50vw;
+              height: 500px;
             `}
           >
-            <svg
-              ref={refPlan}
-              css={css`
-                position: absolute;
-                bottom: 0;
-              `}
-              viewBox={`-10 ${minmax(-500, 10, 50 - co2e * 5)} ${minmax(
-                120,
-                500,
-                yearsToZero(co2e) * 5 + 20
-              )} ${minmax(60, 200, co2e * 5 + 20)}`}
-              id="curve"
-            >
-              <g id="grafic">
-                <text
-                  x="-30"
-                  y="-3"
-                  transform="rotate(-90)"
-                  style={{ fontSize: 7 }}
-                >
-                  co2e
-                </text>
-                <text x="50" y={y(-3)} style={{ fontSize: 7 }}>
-                  years
-                </text>
-                <path d="M0,-500 L0,65 M-5,60 L500,60" stroke="#555" />
-                <path
-                  d={`M0,${y(co2e)} L${yearsToZero(co2e) * 5},60 L0,60 Z`}
-                  stroke="#6af"
-                  fill="#6af3"
-                />
-                <text y="40" style={{ fontSize: 7, fill: '#6af' }}>
-                  <tspan x="5" dy="1.2em">
-                    52,5t
-                  </tspan>
-                  <tspan x="5" dy="1.3em" style={{ fontSize: 4.5 }}>
-                    co2e
-                  </tspan>
-                </text>
-                <path
-                  d={`M5,${60 - tCO2 * 5} L90,${60 - tCO2 * 5}`}
-                  stroke="#aaa"
-                />
-                <rect
-                  x="39"
-                  y={50 - tCO2 * 5}
-                  width="15"
-                  height="15"
-                  fill="white"
-                />
-                <text x="40" y={66 - tCO2 * 5}>
-                  {flag}
-                </text>
-                <path
-                  d={`M5,${60 - tCO2World * 5} L90,${60 - tCO2World * 5}`}
-                  stroke="#aaa"
-                />
-                <rect
-                  x="39"
-                  y={50 - tCO2World * 5}
-                  width="18"
-                  height="18"
-                  fill="white"
-                />
-                <text x="40" y={66 - tCO2World * 5}>
-                  {flagWorld}
-                </text>
-              </g>
-            </svg>
+            <Graph
+              co2e={co2e}
+              endDate={endDate}
+              countryData={countryData}
+              worldData={worldData}
+            />
           </div>
           <p>
-            You need to reach 0 co2e at{' '}
-            {format('dd.MM.yyyy', addDays(yearsToZero(co2e) * 365, new Date()))}{' '}
-            in {yearsToZero(co2e).toFixed(2)} years
+            {formattedEndDate} You need to reach 0 co2e at in{' '}
+            {yearsToZero(co2e).toFixed(2)} years
           </p>
         </Container>
       </Section>
       <Section
         css={css`
-          ${planVisible < 0.3 ?? 'display: none;'}
+          /* ${planVisible < 0.3 ?? 'display: none;'} */
           position: fixed;
           bottom: 0;
           width: 100%;
           padding: 0 0 2em;
           background: #eee;
-          opacity: ${planVisible > 0.4 ? 1 : 0};
+          /* opacity: ${planVisible > 0.4 ? 1 : 0}; */
           transition: opacity 200ms;
 
           z-index: 99;
